bdbee5f115f357c44f5cccd456f3b81bab7e0967
stephenawuah <15618895-stephenawuah@users.noreply.replit.com>
2023-04-11 03:44:29 +0000

    commited

--- myApp.js
+++ myApp.js
@@ -1,55 +1,144 @@
 require('dotenv').config();
+const mySecret = process.env['MONGO_URI']
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+const validator = require('validator');
+
+const personSchema = new Schema({
+    name: String,
+    age: Number,
+    favoriteFoods: [String],
+});
+const Person = mongoose.model('Person' , personSchema);
+
+mongoose.connect(mySecret, { useNewUrlParser: true, useUnifiedTopology: true } , console.log('mongoDB is connected')
+);
 
 
-let Person;
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+const createAndSavePerson = function (done) {
+    const person = new Person({
+        name:'Stephen Awuah',
+        age:20,
+        favoriteFoods:['apple' ,'burrito']
+    });
+    
+    person.save( (err, data) => {
+       if(err) return console.error(err)
+       done(null , data);
+       console.log(data)        
+   });
 };
+ 
+
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+    var people = [
+     {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+     {name: "Sol", age: 76, favoriteFoods: ["burrito"]},
+     {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+    ];
+
+    //var createManyPeople = function(arrayOfPeople, done) {
+     Person.create(arrayOfPeople,  (err, people)=> {
+    if (err) return console.log(err);
+    done(null, people);
+     });
+   // };
 };
 
+
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+    Person.find({ name: personName }, function(err, people) {
+    if (err) {
+      return console.log(err);
+    }
+    done(null, people);
+  });
+    
+ 
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+
+    
+    Person.findOne({favoriteFoods: food}, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+    });
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+    Person.findById(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+    });
+    //done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
 };
+
+
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+     Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true},(err, updatedDoc) =>{
+    if(err) return console.log(err);
+    done(null, updatedDoc);
+    })
 
-  done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+    Person.findByIdAndRemove(personId , function (err, data) {
+    if (err) return console.log(err);
+    done(null, data)
+    })
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+    Person.remove({name: nameToRemove}, (err, response) => {
+    if(err) return console.log(err);
+    done(null, response);
+    })
 };
 
 const queryChain = (done) => {
-  const foodToSearch = "burrito";
+    const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+      Person
+    .find({ favoriteFoods: foodToSearch })
+    .sort({name : 'asc'})
+    .limit(2)
+    .select('-age')
+    .exec(function(err, data) {
+      if (err) {
+        return done(err);
+      }
+      done(null, data);
+    });
+
 };
+
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
